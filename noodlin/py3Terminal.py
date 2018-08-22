c5d1eae1d174da0555e476cd57e3a7577a927c7b
mzc9 <m0nadd0g@gmail.com>
2018-08-21 21:58:56 -0400

    py3 in the terminal

---
+++ noodlin/py3Terminal.py
@@ -0,0 +1,12 @@
+# testing a python3 program in the terminal
+
+# input 2 numbers, add, multiply together and print them out
+
+num_str1 = input('Please enter an integer: ')
+num_str2 = input('Please enter a floating point number: ')
+
+int1 = int(num_str1)
+float1 = float(num_str2)
+
+print('The numbers are: ',int1,' and ',float1)
+print('Their sum is: ',int1+float1, 'Their product is:',int1*float1)
